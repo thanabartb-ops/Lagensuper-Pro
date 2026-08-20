e8b1cbcf9aee385883fdaf2fec3b03b5ab0ed4ba
SERREZHUB <thanabartb@gmail.com>
2026-08-21 00:22:36 +0700

    feat(pro-r2): wrap routes in shared application shell

--- lsuperagent-pro/app/layout.tsx
+++ lsuperagent-pro/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { ReactNode } from "react";
+import { AppShell } from '@/components/shell/AppShell'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,7 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full">
+        <AppShell>{children}</AppShell>
+      </body>
     </html>
   );
 }
