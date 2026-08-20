770ad365e1c96fbc4d23e37c1ce9515c373c6914
SERREZHUB <thanabartb@gmail.com>
2026-08-21 00:26:08 +0700

    feat(pro-r2): add projects module foundation

---
+++ lsuperagent-pro/app/projects/page.tsx
@@ -0,0 +1,5 @@
+import { ModuleStatusPage } from '@/components/modules/ModuleStatusPage'
+
+export default function ProjectsPage() {
+  return <ModuleStatusPage title="Projects" description="Project view foundation only. No remote project mutation is enabled in PRO-R2." />
+}
